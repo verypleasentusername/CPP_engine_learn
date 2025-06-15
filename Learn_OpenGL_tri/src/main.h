bf3d8742a1348168a2ceac8c87f7ca4d23581486
SingleError <isaaconeoneone@gmail.com>
2025-06-15 12:40:13 +0300

    smth broken XD

---
+++ Learn_OpenGL_tri/src/main.h
@@ -0,0 +1,7 @@
+#include "../include/ThirdParty/glad/glad.h"
+#include "../include/ThirdParty/GLFW/glfw3.h"
+
+#include "utils/Shader_util.h"
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
