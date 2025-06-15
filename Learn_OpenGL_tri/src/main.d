26078211d3f4f7f306807fa9b0b775aafbba8420
SingleError <isaaconeoneone@gmail.com>
2025-06-15 15:17:43 +0300

    compile fix, has some problems still
    shader compilation is either broken, in any case the triangle is black

--- Learn_OpenGL_tri/src/main.d
+++ Learn_OpenGL_tri/src/main.d
@@ -1,3 +1,3 @@
 src/main.o: src/main.cpp src/../include/ThirdParty/glad/glad.h \
- src/../include/ThirdParty/GLFW/glfw3.h src/../src/Shader_util.h \
- src/../src/../include/ThirdParty/glad/glad.h
+ src/../include/ThirdParty/GLFW/glfw3.h src/utils/Shader_util.h \
+ src/utils/../../include/ThirdParty/glad/glad.h
